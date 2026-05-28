6baf4df50ca3baf42b41a36b67e4d6c928e61eff
amatunirobert59-byte <amatunirobert59@gmail.com>
2026-05-28 16:14:20 +0300

    fix yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,37 +78,40 @@ export default function RootLayout({
 
         {/* YANDEX METRIKA */}
 
-        <Script id="yandex-metrika" strategy="afterInteractive">
-          {`
-            (function(m,e,t,r,i,k,a){
-              m[i]=m[i]||function(){
-                (m[i].a=m[i].a||[]).push(arguments)
-              };
+        <Script
+  id="yandex-metrika"
+  strategy="afterInteractive"
+>
+  {`
+    (function(m,e,t,r,i,k,a){
+      m[i]=m[i]||function(){
+        (m[i].a=m[i].a||[]).push(arguments)
+      };
 
-              m[i].l=1*new Date();
+      m[i].l=1*new Date();
 
-              for (var j = 0; j < document.scripts.length; j++) {
-                if (document.scripts[j].src === r) {
-                  return;
-                }
-              }
+      for (var j = 0; j < document.scripts.length; j++) {
+        if (document.scripts[j].src === r) {
+          return;
+        }
+      }
 
-              k=e.createElement(t),
-              a=e.getElementsByTagName(t)[0],
-              k.async=1,
-              k.src=r,
-              a.parentNode.insertBefore(k,a)
+      k=e.createElement(t),
+      a=e.getElementsByTagName(t)[0],
+      k.async=1,
+      k.src=r,
+      a.parentNode.insertBefore(k,a)
 
-            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-            ym(109472158, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true
-            });
-          `}
-        </Script>
+    ym(109472158, "init", {
+      clickmap:true,
+      trackLinks:true,
+      accurateTrackBounce:true,
+      webvisor:true
+    });
+  `}
+</Script>
 
       </body>
     </html>
